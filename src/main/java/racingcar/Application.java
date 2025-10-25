3720f3eee0c6eeeef4eb46d6c9c287adfd261fb4
mmm307955 <mmm307955@gmail.com>
2025-10-25 12:29:01 +0900

    feat: 사용자로부터 시도 횟수 입력 기능 추가
    - 사용자가 이동 횟수를 입력할 수 있다.
    - 잘못된 입력값일 경우 IllegalArgumentException을 발생시킨다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -11,6 +11,9 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         setCarsNames();
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int round = setRoundTimes();
     }
 
     public static void setCarsNames() {
@@ -38,5 +41,23 @@ public class Application {
             throw new IllegalArgumentException("빈 이름은 입력할 수 없습니다. ");
         }
         return strippedName;
+    }
+
+    public static int setRoundTimes() {
+        String input = Console.readLine().strip();
+        return validateRound(input);
+    }
+
+    public static int validateRound(String input) {
+        try {
+            int round = Integer.parseInt(input);
+            if (round < 0) {
+                throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
+            }
+            System.out.println();
+            return round;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("올바른 시도 횟수를 입력해주세요.(숫자만 입력 가능하며 2,147,483,647 이하의 숫자만 허용됩니다.)");
+        }
     }
 }
