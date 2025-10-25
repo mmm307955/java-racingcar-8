3cdbca5d715569af7082f727a6cefbb8353ac787
mmm307955 <mmm307955@gmail.com>
2025-10-25 12:17:27 +0900

    feat: 자동차 이름 입력 기능 추가
    - 자동차 이름을 쉼표(,)로 구분하여 입력받는다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,29 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.domain.Car;
+
 public class Application {
+    private static final List<Car> cars = new ArrayList<>();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        setCarsNames();
+    }
+
+    public static void setCarsNames() {
+        String[] eachCarNames = splitCarNames();
+
+        for (String name : eachCarNames) {
+            Car car = new Car();
+            car.setName(name);
+            cars.add(car);
+        }
+    }
+
+    public static String[] splitCarNames() {
+        return Console.readLine().split(",");
     }
 }
--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,4 +2,8 @@ package racingcar.domain;
 
 public class Car {
     private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
