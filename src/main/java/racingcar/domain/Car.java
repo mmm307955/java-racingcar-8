4e5e37cde490b686c774fbea859ed220a6ad644b
mmm307955 <mmm307955@gmail.com>
2025-10-25 12:18:44 +0900

    feat: Car 클래스에 position 필드 추가
    - 자동차의 전진 횟수를 기록하기 위한 position 필드를 추가한다.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,8 +2,21 @@ package racingcar.domain;
 
 public class Car {
     private String name;
+    private int position;
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
     }
 }
