01b19a0ded42b25d7bf185e9b834ae0983d4b488
mmm307955 <mmm307955@gmail.com>
2025-10-27 13:06:06 +0900

    test: ApplicationTest에 기능 및 예외 테스트 추가
    - 각 자동차에 이름을 부여할 수 있다.
    - 이름이 5글자 이상이면 예외를 발생시킨다.
    - 주어진 횟수 동안 n대의 자동차는 전진 또는 멈출 수 있다.
    - 시도 횟수가 올바르지 않으면 예외를 발생시킨다.

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,7 +1,10 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -15,24 +18,85 @@ class ApplicationTest extends NsTest {
     @Test
     void 기능_테스트() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
     @Override
     public void runMain() {
         Application.main(new String[]{});
+    }
+
+
+    @DisplayName("각 자동차에 이름을 부여할 수 있다")
+    @Test
+    void 각_자동차에_이름을_부여할_수_있다() {
+        Car car = new Car();
+        car.setName("pobi");
+        assertThat(car.getName()).isEqualTo("pobi");
+    }
+
+    @DisplayName("이름이 5글자 이상이면 예외를 발생시킨다")
+    @Test
+    void 이름이_5글자_이상이면_예외를_발생시킨다() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @DisplayName("주어진 횟수 동안 n대의 자동차는 전진 또는 멈출 수 있다")
+    @Test
+    void 주어진_횟수_동안_n대의_자동차는_전진_또는_멈출_수_있다() {
+        // given
+        Car pobi = new Car();
+        pobi.setName("pobi");
+
+        Car java = new Car();
+        java.setName("java");
+
+        Car woni = new Car();
+        woni.setName("woni");
+
+        List<Car> cars = List.of(pobi, java, woni);
+        int round = 5;
+
+        // when
+        for (int i = 0; i < round; i++) {
+            for (Car car : cars) {
+                if (Application.isOverFour()) {
+                    car.setPosition(car.getPosition() + 1);
+                }
+            }
+        }
+
+        // then
+        for (int i = 0; i < round; i++) {
+            for (Car car : cars) {
+                assertThat(car.getPosition()).isGreaterThan(0).isLessThan(round);
+            }
+        }
+    }
+
+
+    @DisplayName("시도 횟수가 올바르지 않으면 예외를 발생시킨다")
+    @Test
+    void 시도_횟수가_올바르지_않으면_예외를_발생시킨다() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,java", "다섯번"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
     }
 }
